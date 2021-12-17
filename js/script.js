89f4a8b5fb554975cfa1256f48a1055b1ceb2232
Rafael Alcantara <rafael.alcantara.dev@gmail.com>
2021-12-17 11:08:44 -0300

    Fetch error is fixed

--- js/script.js
+++
@@ -1,6 +0,0 @@
-// let date = new Date();
-// let currentYear = date.getFullYear();
-// document.querySelector('#current-year').textContent = currentYear;
-
-// let lastModified = document.lastModified;
-// document.getElementById('last-modified').textContent = lastModified;
